961c49ff421ab62cda4cdceb1a0584ae5d20f192
biscaboy <david@davidjdickinson.com>
2022-05-04 11:23:09 -0700

    feat: resize images

--- coaches.php
+++ coaches.php
@@ -9,7 +9,7 @@
                     	<h2><a name="ee"></a>Elaine "EE" Moe</h2>
                         <h3>Artistic Director, Lead Coach</h3>
                         <div id="instructortext">
-                        <div id="instructorphoto"><img class="headshot" src="photos/ee-2022.jpg" width="240" /> </div>
+                        <div id="instructorphoto"><img class="headshot" src="photos/ee-2022.jpg" width="200" /> </div>
                         <p>
                         	Elaine “E.E.” Moe (Artistic Director/Lead Coach) AZ/CA career spans 55+ years in the arts, education, corporate and entertainment industry.  Critical honors for decades producing, directing, acting, civic and cultural engagement reflect her lifelong dedication advocating, mentoring and engaging artists parity in theatre & film through Diversity, Equity and Inclusion in all mediums.
 
@@ -33,7 +33,7 @@
                         <h2><a name="dd"></a>David Dickinson</h2>
                         <h3>Artist in Residence/Artistic Associate</h3>
                         <div id="instructortext">
-                            <div id="instructorphoto"><img class="headshot" src="photos/dd-2022.jpg"  width="206"/> </div>
+                            <div id="instructorphoto"><img class="headshot" src="photos/dd-2022.jpg"  width="127"/> </div>
                             <p>David has been privileged to be a part of the Arizona theatre community since 2005 performing for A/C Theatre Company, Phoenix Theatre, Actors Theatre of Phoenix, Childsplay, Southwest Shakespeare, Shakespeare Sedona, Class 6 Theatre and Desert Foothills Theatre. He recently spent three years as a proud member of Phoenix Theatre's <i>Partners That Heal</i> which entertains and sooths patients at the Phoenix Children's Hospital and the Banner Thunderbird Medical Center. For seven seasons David toured regionally and internationally with Childsplay, Speeltheater Holland and La Jolla Playhouse delighting young audiences in over 1000 stage performances. He has directed two stage productions with Class 6 Theatre at the Mesa Arts Center.  On camera, David has appeared in regional commercials, film and TV as the host of the local teen cooking show <i>Food*Ball</i> appearing on AZTV7.  David is also passionate about the script development process and has been involved as an actor in the development of new works through the Write Now! national play development conference, the Phoenix Theatre Festival of New American Theatre, and several development workshops at Childsplay including <i>Rock Paper Scissors, The Miraculous Journey of Edward Tulane, Recipe for Disaster, The Three Javelinas</i> and <i>Chato's Kitchen.</i>&nbsp; He enjoys playing live music on stage and appears sometimes with his violin, ukulele, harmonica or banjo.&nbsp; Film and commercial representation: <a href="http://www.fordrba.com">Ford Robert Black Agency.</a>&nbsp; Recent credits: <i>Something Wicked This Way Comes</i> (BLK BOX PHX), <i>Gunmetal Blues</i> (A/C Theatre Co.), <i>Twist Your Dickens</i> and <i>Bullets Over Broadway </i>(Phoenix Theatre),&nbsp; <i>Merchant</i><i> of Venice</i> and <i>Wittenberg</i> (Southwest Shakespeare).&nbsp; <a href="http://www.davidjdickinson.com">www.davidjdickinson.com</a></p>
                         </div>
 
@@ -42,7 +42,7 @@
                         <h2><a name="pf"></a>Pamela Fields</h2>
                         <h3>Master Classes Consultant, Acting</h3>
                         <div id="instructortext">
-                            <div id="instructorphoto"><img class="headshot" src="photos/pam.jpg" /> </div>
+                            <div id="instructorphoto"><img class="headshot" src="photos/pam.jpg" width="127"/> </div>
                             <p>A professional educator, actor and director, Pam was the Theatre Arts Director at Scottsdale Community College from 1975-2002, and her directing activities there earned extensive recognition. Award-winning productions included <em>Ladyhouse Blues, Blue Window</em> and the acclaimed <em>Working,</em> which enjoyed invitational performances at the Kennedy Center in Washington, D.C. Active in the Kennedy Center/American College Theatre Festival at the national, regional and state levels, Pam earned three Kennedy Center Medals for Excellence in Theatre Education and membership in the American College Theatre Festival Fellows for Distinguished Service.</p>
 
                             <p>Pam was also the founding Director of SCC’s unique summer performance training program, Scottsdale Conservatory Theatre, for which she was awarded SCC Innovator of the Year.</p>
@@ -60,7 +60,7 @@
                         <h2><a name="jb"></a>Jamie Bauer Spano</h2>
                         <h3>Associate Artist</h3>
                         <div id="instructortext">
-                            <div id="instructorphoto"><img class="headshot" src="photos/jamie.jpg"  width="206"/> </div>
+                            <div id="instructorphoto"><img class="headshot" src="photos/jamie.jpg"  width="127"/> </div>
                             <p>Jamie Bauer Spano is an actor, director and teaching artist. She began her journey with the Megaw Actors studio in 2003 and is proud to now continue her professional applications of the craft with those that have been an intrical part of her training. In addition to her training with the Megaw, Jamie holds a BA in Theatre from Metropolitan State University of Denver. Jamie has been working diligently over the last ten years with youth theatre programs in both Phoenix and Denver. She has worked with Kaiser Permanente's nationally recognized Educational Theatre Programs as a touring actor, facilitator, and project coordinator. She developed and taught a number of classes and outreach with the City of Aurora and was a member of VisionBox Actors Studio while in the Denver area. Locally, Jamie has taught with Limelight Youth Theatre, Mesa Encore Theatre, and Southwest Shakespeare’s Teen Conservatory. She has been awarded two AriZonis and a National Youth Arts award for Outstanding Direction and Overall Production 2017/2018 (Youth Plays: Radium Girls and The Sparrow). Recent Stage credits include performances with The Hale Center Theatre, Stray Cat Theatre (AriZoni nomination supporting actress) and Southwest Shakespeare Company. </p>
                         </div>
                     </div>
@@ -68,7 +68,7 @@
                         <h2><a name="ld"></a>Laura Durant</h2>
                         <h3>Publicist/Guest Artist</h3>
                     	<div id="instructortext">
-                            <div id="instructorphoto"><img class="headshot" src="photos/laura.jpg"  /> </div>
+                            <div id="instructorphoto"><img class="headshot" src="photos/laura.jpg" width="127" /> </div>
                             <p>Laura Durant is a Guest Artist and presents the very informative "Actors Toolbox."  Her casting website, <a id="exlink" href="http://www.durantcom.com" target="_blank">DurantCom.com</a>, is the "go-to" resource for actors in the Metro Phoenix area. Laura is also an award-winning film and stage actor, voice over talent, writer and photographer.
                         </p>
                         </div>
